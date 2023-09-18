59340f18a5c7a2f924c34cae677aafee704dd9e4
Dipayan Maji <dipayanmaji1112@gmail.com>
2023-09-18 17:13:12 +0530

    add more social media

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -31,9 +31,11 @@ const Footer = () => {
                 </div>
 
                 <div className="right-part">
-                    <a href="https://www.facebook.com/dip.ayan.716" target="_blank"><i className="fa-brands fa-facebook-f"></i></a>
-                    <a href="https://twitter.com/dipayanmaji11" target="_blank"><i className="fa-brands fa-x-twitter"></i></a>
-                    <a href="https://www.linkedin.com/in/dipayanmaji/" target="_blank"><i className="fa-brands fa-linkedin-in"></i></a>
+                    <a href='https://www.linkedin.com/in/dipayanmaji/' target='_blank'><i className="fa-brands fa-linkedin-in"></i></a>
+                    <a href='https://github.com/dipayanmaji' target='_blank'><i className="fa-brands fa-github"></i></a>
+                    <a href='https://twitter.com/dipayanmaji11' target='_blank'><i className="fa-brands fa-x-twitter"></i></a>
+                    <a href='https://www.instagram.com/dipayan.maji/' target='_blank'><i className="fa-brands fa-instagram"></i></a>
+                    <a href='https://www.facebook.com/dip.ayan.716' target='_blank'><i className="fa-brands fa-facebook-f"></i></a>
                 </div>
             </div>
         </footer>
